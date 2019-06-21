96fa968675dd7ab0386e28e2ac29e21a4af1dbcc
Bass <jonathan.bass.ext@siemens.com>
2019-06-21 13:52:05 +0100

    initial commit

---
+++ OpenFaceApi/Controllers/OpenFaceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenFaceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenFaceController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(string images)
+        {
+            var result = new List<string> {$"Scanning for images in images/examples/{images}"};
+
+            var process = new Process();
+            var startinfo = new ProcessStartInfo("run_openface.bat")
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                Arguments = images
+            };
+
+            process.StartInfo = startinfo;
+            
+            process.Start();
+            process.WaitForExit();
+
+            var outputText = process.StandardOutput.ReadToEnd();
+            var outputLines = SplitActualOutputLines(outputText);
+
+            result.AddRange(outputLines);
+
+            return result;
+        }
+
+        private static IEnumerable<string> SplitActualOutputLines(string text)
+        {
+            return text.Split(Environment.NewLine)
+                .Where(line => line != string.Empty && !line.StartsWith(@"C:\"));
+        }
+    }
+}
---
+++ OpenFaceApi/run_openface.bat
@@ -0,0 +1,5 @@
+﻿cd "C:\Program Files\Docker\Docker\resources\bin"
+
+SET @searchTerm=%*
+
+Docker.exe exec -it bc4f49e5cf7c /bin/bash -c "cd root/openface/;demos/compare.py images/examples/%@searchTerm%"
